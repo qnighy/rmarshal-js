4a698c49b098e05c97f333916dba35400c6f7aac
Masaki Hara <ackie.h.gmai@gmail.com>
2024-12-14 22:28:29 +0900

    Add parse.ts

---
+++ parse.test.ts
@@ -0,0 +1,286 @@
+import { assertEquals, assertThrows } from "@std/assert";
+import {
+  MarshalBoolean,
+  MarshalInteger,
+  MarshalNil,
+  type MarshalValue,
+} from "./ast.ts";
+import { parse } from "./parse.ts";
+import { seq, type SeqElement } from "./testutil.ts";
+
+function p(...elems: SeqElement[]): MarshalValue {
+  return parse(seq(...elems));
+}
+
+Deno.test("parse parses something", () => {
+  assertEquals(p("\x04\x08", "0"), MarshalNil());
+});
+
+Deno.test("parse rejects short or long data", () => {
+  assertThrows(() => p("\x04\x08"), SyntaxError, "Unexpected end of input");
+  assertThrows(
+    () => p("\x04\x08", "0", "0"),
+    SyntaxError,
+    "Unexpected trailing data",
+  );
+});
+
+Deno.test("parse rejects invalid versions", () => {
+  assertThrows(
+    () => p("\x03\x08", "0"),
+    SyntaxError,
+    "Unsupported marshal version: 3.8 (expected 4.0 to 4.8)",
+  );
+  assertThrows(
+    () => p("\x05\x08", "0"),
+    SyntaxError,
+    "Unsupported marshal version: 5.8 (expected 4.0 to 4.8)",
+  );
+  assertThrows(
+    () => p("\x04\x09", "0"),
+    SyntaxError,
+    "Unsupported marshal version: 4.9 (expected 4.0 to 4.8)",
+  );
+  assertEquals(p("\x04\x07", "0"), MarshalNil());
+});
+
+Deno.test("parse parses nil", () => {
+  assertEquals(p("\x04\x08", "0"), MarshalNil());
+});
+
+Deno.test("parse parses boolean", () => {
+  assertEquals(p("\x04\x08", "F"), MarshalBoolean(false));
+  assertEquals(p("\x04\x08", "T"), MarshalBoolean(true));
+});
+
+Deno.test("parse parses Fixnum zero", () => {
+  assertEquals(p("\x04\x08", "i\x00"), MarshalInteger(0n));
+});
+
+Deno.test("parse parses Fixnum in positive short form", () => {
+  assertEquals(p("\x04\x08", "i\x06"), MarshalInteger(1n));
+  assertEquals(p("\x04\x08", "i\x7F"), MarshalInteger(122n));
+});
+
+Deno.test("parse rejects Fixnum non-canonical positive short form of zero", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\x05"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse parses Fixnum in negative short form", () => {
+  assertEquals(p("\x04\x08", "i\xFA"), MarshalInteger(-1n));
+  assertEquals(p("\x04\x08", "i\x80"), MarshalInteger(-123n));
+});
+
+Deno.test("parse rejects Fixnum non-canonical negative short form of zero", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\xFB"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse parses Fixnum in positive 1-byte form", () => {
+  assertEquals(p("\x04\x08", "i\x01\x7B"), MarshalInteger(123n));
+  assertEquals(p("\x04\x08", "i\x01\xFF"), MarshalInteger(255n));
+});
+
+Deno.test("parse rejects Fixnum redundant positive 1-byte form (0 to 122)", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\x01\x00"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\x01\x7A"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse parses Fixnum in negative 1-byte form", () => {
+  assertEquals(p("\x04\x08", "i\xFF\x84"), MarshalInteger(-124n));
+  assertEquals(p("\x04\x08", "i\xFF\x00"), MarshalInteger(-256n));
+});
+
+Deno.test("parse rejects Fixnum redundant negative 1-byte form (-123 to -1)", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\xFF\x85"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\xFF\xFF"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse parses Fixnum in Fixnum in positive 2-byte form", () => {
+  assertEquals(p("\x04\x08", "i\x02\x00\x01"), MarshalInteger(256n));
+  assertEquals(p("\x04\x08", "i\x02\xFF\xFF"), MarshalInteger(0xFFFFn));
+});
+
+Deno.test("parse rejects Fixnum redundant positive 2-byte form (0 to 255)", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\x02\x00\x00"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\x02\xFF\x00"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse parses Fixnum in negative 2-byte form", () => {
+  assertEquals(p("\x04\x08", "i\xFE\xFF\xFE"), MarshalInteger(-257n));
+  assertEquals(p("\x04\x08", "i\xFE\x00\x00"), MarshalInteger(-0x10000n));
+});
+
+Deno.test("parse rejects Fixnum redundant negative 2-byte form (-256 to -1)", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\xFE\xFF\xFF"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\xFE\x00\xFF"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse parses Fixnum in positive 3-byte form", () => {
+  assertEquals(p("\x04\x08", "i\x03\x00\x00\x01"), MarshalInteger(0x10000n));
+  assertEquals(p("\x04\x08", "i\x03\xFF\xFF\xFF"), MarshalInteger(0xFFFFFFn));
+});
+
+Deno.test("parse rejects Fixnum redundant positive 3-byte form (0 to 65535)", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\x03\x00\x00\x00"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\x03\xFF\xFF\x00"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse parses Fixnum in negative 3-byte form", () => {
+  assertEquals(p("\x04\x08", "i\xFD\xFF\xFF\xFE"), MarshalInteger(-0x10001n));
+  assertEquals(p("\x04\x08", "i\xFD\x00\x00\x00"), MarshalInteger(-0x1000000n));
+});
+
+Deno.test("parse rejects Fixnum redundant negative 3-byte form (-65536 to -1)", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\xFD\xFF\xFF\xFF"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\xFD\x00\x00\xFF"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse parses Fixnum in positive 4-byte form", () => {
+  assertEquals(
+    p("\x04\x08", "i\x04\x00\x00\x00\x01"),
+    MarshalInteger(0x1000000n),
+  );
+  assertEquals(
+    p("\x04\x08", "i\x04\xFF\xFF\xFF\x3F"),
+    MarshalInteger(0x3FFFFFFFn),
+  );
+});
+
+Deno.test("parse rejects Fixnum redundant positive 4-byte form (0 to 16777215)", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\x04\x00\x00\x00\x00"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\x04\xFF\xFF\xFF\x00"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse rejects Fixnum too large for 31bit positive", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\x04\x00\x00\x00\x40"),
+    SyntaxError,
+    "Integer too large for 31bit",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\x04\xFF\xFF\xFF\x7F"),
+    SyntaxError,
+    "Integer too large for 31bit",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\x04\x00\x00\x00\x80"),
+    SyntaxError,
+    "Integer too large for 32bit",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\x04\xFF\xFF\xFF\xFF"),
+    SyntaxError,
+    "Integer too large for 32bit",
+  );
+});
+
+Deno.test("parse parses Fixnum in negative 4-byte form", () => {
+  assertEquals(
+    p("\x04\x08", "i\xFC\xFF\xFF\xFF\xFE"),
+    MarshalInteger(-0x1000001n),
+  );
+  assertEquals(
+    p("\x04\x08", "i\xFC\x00\x00\x00\xC0"),
+    MarshalInteger(-0x40000000n),
+  );
+});
+
+Deno.test("parse rejects Fixnum redundant negative 4-byte form (-16777216 to -1)", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\xFC\xFF\xFF\xFF\xFF"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\xFC\x00\x00\x00\xFF"),
+    SyntaxError,
+    "Non-canonical Fixnum representation",
+  );
+});
+
+Deno.test("parse rejects Fixnum too large for 31bit negative", () => {
+  assertThrows(
+    () => p("\x04\x08", "i\xFC\xFF\xFF\xFF\xBF"),
+    SyntaxError,
+    "Integer too large for 31bit",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\xFC\x00\x00\x00\x80"),
+    SyntaxError,
+    "Integer too large for 31bit",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\xFC\xFF\xFF\xFF\x7F"),
+    SyntaxError,
+    "Integer too large for 32bit",
+  );
+  assertThrows(
+    () => p("\x04\x08", "i\xFC\x00\x00\x00\x00"),
+    SyntaxError,
+    "Integer too large for 32bit",
+  );
+});
---
+++ parse.ts
@@ -0,0 +1,240 @@
+import {
+  MarshalBoolean,
+  MarshalInteger,
+  MarshalNil,
+  MarshalValue,
+} from "./ast.ts";
+import {
+  MARSHAL_MAJOR,
+  MARSHAL_MINOR,
+  TYPE_ARRAY,
+  TYPE_BIGNUM,
+  TYPE_CLASS,
+  TYPE_DATA,
+  TYPE_EXTENDED,
+  TYPE_FALSE,
+  TYPE_FIXNUM,
+  TYPE_FLOAT,
+  TYPE_HASH,
+  TYPE_HASH_DEF,
+  TYPE_IVAR,
+  TYPE_LINK,
+  TYPE_MODULE,
+  TYPE_MODULE_OLD,
+  TYPE_NIL,
+  TYPE_OBJECT,
+  TYPE_REGEXP,
+  TYPE_STRING,
+  TYPE_STRUCT,
+  TYPE_SYMBOL,
+  TYPE_SYMLINK,
+  TYPE_TRUE,
+  TYPE_UCLASS,
+  TYPE_USERDEF,
+  TYPE_USRMARSHAL,
+} from "./marshal-common.ts";
+
+export function parse(buf: Uint8Array): MarshalValue {
+  const parser = new Parser(buf);
+  const value = parser.readTopLevel();
+  if (parser.rest().length > 0) {
+    throw new SyntaxError("Unexpected trailing data");
+  }
+  return value;
+}
+export function* parseStream(buf: Uint8Array): IterableIterator<MarshalValue> {
+  const parser = new Parser(buf);
+  while (parser.hasRest()) {
+    yield parser.readTopLevel();
+  }
+}
+export function parseNext(buf: Uint8Array): [MarshalValue, Uint8Array] {
+  const parser = new Parser(buf);
+  const value = parser.readTopLevel();
+  return [value, parser.rest()];
+}
+
+export class Parser {
+  #buf: Uint8Array;
+  #pos = 0;
+
+  constructor(buf: Uint8Array) {
+    this.#buf = buf;
+  }
+
+  hasRest(): boolean {
+    return this.#pos < this.#buf.length;
+  }
+
+  rest(): Uint8Array {
+    return this.#buf.subarray(this.#pos);
+  }
+
+  readTopLevel(): MarshalValue {
+    const major = this.#readByte();
+    const minor = this.#readByte();
+    if (major !== MARSHAL_MAJOR || minor > MARSHAL_MINOR) {
+      throw new SyntaxError(
+        `Unsupported marshal version: ${major}.${minor} (expected ${MARSHAL_MAJOR}.0 to ${MARSHAL_MAJOR}.${MARSHAL_MINOR})`,
+      );
+    }
+    return this.#readValue();
+  }
+
+  #readValue(): MarshalValue {
+    const type = this.#readByte();
+    switch (type) {
+      case TYPE_NIL:
+        return MarshalNil();
+      case TYPE_TRUE:
+        return MarshalBoolean(true);
+      case TYPE_FALSE:
+        return MarshalBoolean(false);
+      case TYPE_FIXNUM:
+        return this.#readFixnumBody();
+      case TYPE_BIGNUM:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_FLOAT:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_SYMBOL:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_SYMLINK:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_IVAR:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_EXTENDED:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_UCLASS:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_OBJECT:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_ARRAY:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_HASH:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_HASH_DEF:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_STRING:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_REGEXP:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_USRMARSHAL:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_USERDEF:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_DATA:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_STRUCT:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_MODULE_OLD:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_CLASS:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_MODULE:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      case TYPE_LINK:
+        throw new Error(`TODO: not implemented yet: ${describeType(type)}`);
+      default:
+        throw new SyntaxError(`Unknown type: ${describeByte(type)}`);
+    }
+  }
+
+  #readFixnumBody(): MarshalInteger {
+    const num = this.#readLong();
+    if (num < -0x40000000 || num >= 0x40000000) {
+      throw new SyntaxError("Integer too large for 31bit");
+    }
+    return MarshalInteger(BigInt(num));
+  }
+
+  #readLong(): number {
+    const first = this.#readByte();
+    if (first === 0) {
+      return 0;
+    } else if (first >= 6 && first <= 127) {
+      // 1 to 122
+      return first - 5;
+    } else if (first >= 128 && first <= 250) {
+      // -123 to -1
+      return first - 251;
+    } else if (first >= 1 && first <= 4) {
+      const numBytes = first;
+      let value = 0;
+      for (let i = 0; i < numBytes; i++) {
+        value = value + this.#readByte() * 256 ** i;
+      }
+      if (value < 123 || value < 256 ** (numBytes - 1)) {
+        throw new SyntaxError("Non-canonical Fixnum representation");
+      } else if (value >= 0x80000000) {
+        throw new SyntaxError("Integer too large for 32bit");
+      }
+      return value;
+    } else if (first >= 252 && first <= 255) {
+      const numBytes = 256 - first;
+      let value = 0;
+      for (let i = 0; i < numBytes; i++) {
+        value = value + this.#readByte() * 256 ** i;
+      }
+      value -= 256 ** numBytes;
+      if (value >= -123 || value >= -(256 ** (numBytes - 1))) {
+        throw new SyntaxError("Non-canonical Fixnum representation");
+      } else if (value < -0x80000000) {
+        throw new SyntaxError("Integer too large for 32bit");
+      }
+      return value;
+    } else {
+      throw new SyntaxError("Non-canonical Fixnum representation");
+    }
+  }
+
+  #readByte(): number {
+    if (this.#pos >= this.#buf.length) {
+      throw new SyntaxError("Unexpected end of input");
+    }
+    return this.#buf[this.#pos++];
+  }
+}
+
+const TYPE_NAMES = new Map<number, string>([
+  [TYPE_NIL, "nil"],
+  [TYPE_TRUE, "true"],
+  [TYPE_FALSE, "false"],
+  [TYPE_FIXNUM, "Integer (Fixnum)"],
+  [TYPE_BIGNUM, "Integer (Bignum)"],
+  [TYPE_FLOAT, "Float"],
+  [TYPE_SYMBOL, "Symbol"],
+  [TYPE_SYMLINK, "Symbol link"],
+  [TYPE_IVAR, "instance variable container"],
+  [TYPE_EXTENDED, "singleton extension"],
+  [TYPE_UCLASS, "subclass of Array/Hash/String/Regexp"],
+  [TYPE_OBJECT, "Object"],
+  [TYPE_ARRAY, "Array"],
+  [TYPE_HASH, "Hash"],
+  [TYPE_HASH_DEF, "Hash with default value"],
+  [TYPE_STRING, "String"],
+  [TYPE_REGEXP, "Regexp"],
+  [TYPE_USRMARSHAL, "#marshal_dump"],
+  [TYPE_USERDEF, "#_dump"],
+  [TYPE_DATA, "#_dump_data"],
+  [TYPE_STRUCT, "Struct or Data"],
+  [TYPE_MODULE_OLD, "Class or Module"],
+  [TYPE_CLASS, "Class"],
+  [TYPE_MODULE, "Module"],
+  [TYPE_LINK, "Object link"],
+]);
+
+function describeType(byte: number): string {
+  const name = TYPE_NAMES.get(byte);
+  if (name) {
+    return name;
+  }
+  return `Unknown type ${describeByte(byte)}`;
+}
+
+function describeByte(byte: number): string {
+  const hex = `0x${byte.toString(16).padStart(2, "0").toUpperCase()}`;
+  if (byte >= 0x20 && byte <= 0x7E) {
+    return `${hex} '${String.fromCharCode(byte)}'`;
+  }
+  return hex;
+}
