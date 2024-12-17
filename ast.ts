4472143c2a65cfe4c208483b6fb4f863f4a68707
Masaki Hara <ackie.h.gmai@gmail.com>
2024-12-17 21:09:58 +0900

    Improve AST defs

--- ast.ts
+++ ast.ts
@@ -26,7 +26,7 @@ export type MarshalValue =
   | MarshalModule;
 
 /**
- * Represents nil, whose identity does not matter.
+ * Represents nil (NilClass), whose identity does not matter.
  */
 export type MarshalNil = {
   type: "NilClass";
@@ -36,7 +36,7 @@ export function MarshalNil(): MarshalNil {
 }
 
 /**
- * Represents true or false, whose identity does not matter.
+ * Represents true (TrueClass) or false (FalseClass), whose identity does not matter.
  */
 export type MarshalBoolean = {
   type: "Boolean";
@@ -47,9 +47,9 @@ export function MarshalBoolean(value: boolean): MarshalBoolean {
 }
 
 /**
- * Represents an integer.
+ * Represents an integer (Integer, formerly known as Fixnum/Bignum).
  *
- * For values between -0x40000000 and 0x3FFFFFFF,
+ * For values between -0x40000000 and 0x3FFFFFFF (i.e. 32bit Fixnum),
  * identity does not matter.
  */
 export type MarshalInteger = {
@@ -61,7 +61,7 @@ export function MarshalInteger(value: bigint): MarshalInteger {
 }
 
 /**
- * Represents a floating-point number.
+ * Represents a floating-point number (Float).
  */
 export type MarshalFloat = {
   type: "Float";
@@ -83,83 +83,161 @@ export function MarshalSymbol(value: RSymbol): MarshalSymbol {
 }
 
 /**
- * Represents a plain object.
+ * Represents a plain object, which is any instance of BasicObject
+ * or its subclass other than:
+ *
+ * - nil (NilClass)
+ * - true (TrueClass) / false (FalseClass)
+ * - Integer
+ * - Float
+ * - Symbol
+ * - Array
+ * - Hash
+ * - String
+ * - Regexp
+ * - Those that implement #marshal_dump or #_dump
+ * - Those that contains extension-defined data (which must implement #_dump_data)
+ * - Struct or Data (>= Ruby 3.2)
+ * - Module or Class
+ *
+ * The object may contain cyclic references through ivars.
  */
 export type MarshalObject = {
   type: "Object";
+  /**
+   * The fully-qualified class name of the object.
+   */
   className: RSymbol;
+  /**
+   * Instance variables of the object, in the order of definition.
+   *
+   * For Ruby-defined or extension-defined classes, it may contain ivars
+   * whose names do not start with `@`.
+   * For example, Range objects have ivars named `begin`, `end`, and `excl`.
+   */
   ivars: Map<RSymbol, MarshalValue>;
+  /**
+   * The fully qualified class names of the modules
+   * that the object's singleton class includes,
+   * in the order of Marshal declaration (i.e. in the reverse order of inclusion).
+   */
   extenders: RSymbol[];
-  /** true if it is self-referential. */
-  cycle: boolean | undefined;
 };
 export type MarshalObjectOptions = {
   extenders?: RSymbol[] | undefined;
-  cycle?: boolean | undefined;
 };
 export function MarshalObject(
   className: RSymbol,
   ivars: Map<RSymbol, MarshalValue>,
   options: MarshalObjectOptions = {},
 ): MarshalObject {
-  const { extenders = [], cycle } = options;
-  return { type: "Object", className, ivars, extenders, cycle };
+  const { extenders = [] } = options;
+  return { type: "Object", className, ivars, extenders };
 }
 
 /**
  * Represents an instance of Array
  * or its subclass.
+ *
+ * The object may contain cyclic references through its array elements
+ * or ivars.
  */
 export type MarshalArray = {
   type: "Array";
+  /**
+   * The elements of the array.
+   */
   elements: MarshalValue[];
   /**
+   * The fully-qualified class name of the object.
+   *
    * Missing className indicates that the object is
    * a direct instance of Array.
    */
   className: RSymbol | undefined;
+  /**
+   * Instance variables of the object, in the order of definition.
+   */
   ivars: Map<RSymbol, MarshalValue>;
+  /**
+   * The fully qualified class names of the modules
+   * that the object's singleton class includes,
+   * in the order of Marshal declaration (i.e. in the reverse order of inclusion).
+   */
   extenders: RSymbol[];
-  /** true if it is self-referential. */
-  cycle: boolean | undefined;
 };
 export type MarshalArrayOptions = {
   className?: RSymbol | undefined;
   ivars?: Map<RSymbol, MarshalValue> | undefined;
   extenders?: RSymbol[] | undefined;
-  cycle?: boolean | undefined;
 };
 export function MarshalArray(
   elements: MarshalValue[],
   options: MarshalArrayOptions = {},
 ): MarshalArray {
-  const { className, ivars = new Map(), extenders = [], cycle } = options;
-  return { type: "Array", elements, className, ivars, extenders, cycle };
+  const { className, ivars = new Map(), extenders = [] } = options;
+  return { type: "Array", elements, className, ivars, extenders };
 }
 
 /**
  * Represents an instance of Hash
  * or its subclass.
+ *
+ * The object may contain cyclic references through its keys, values,
+ * the default value, or ivars.
  */
 export type MarshalHash = {
   type: "Hash";
+  /**
+   * The entries of the hash, in the order of insertion.
+   */
   entries: [MarshalValue, MarshalValue][];
+  /**
+   * The default value of the hash, or undefined if
+   * there is no default value.
+   *
+   * The value, if any, should not be nil, because regular
+   * Ruby operation cannot yield Hash with nil default value.
+   * Instead, setting the default value through Hash#default= method
+   * results in a Hash without default value.
+   */
   defaultValue: MarshalValue | undefined;
   /**
-   * Is this a wrapper Hash object generated
-   * for a set of keyword parameters passed
-   * to a Method/Proc with ruby2_keywords flag?
+   * Set to true if the Hash object was created as
+   * a keyword argument container for a Method or
+   * Proc with splat arguments and without keyword parameters
+   * which was marked as ruby2_keywords in Ruby 2.7 or later.
+   *
+   * For example:
+   *
+   * ```ruby
+   * ruby2_keywords def foo(*args)
+   *   Marshal.dump(args.last)
+   * end
+   * foo(a: 1) # => yields Hash with ruby2Keywords=true
+   * ```
+   *
+   * Hashes with ruby2Keywords=true must not set className
+   * as there is no way to generate such Hashes in Ruby.
    */
   ruby2Keywords: boolean;
   /**
+   * The fully-qualified class name of the object.
+   *
    * Missing className indicates that the object is
    * a direct instance of Hash.
    */
   className: RSymbol | undefined;
+  /**
+   * Instance variables of the object, in the order of definition.
+   */
   ivars: Map<RSymbol, MarshalValue>;
+  /**
+   * The fully qualified class names of the modules
+   * that the object's singleton class includes,
+   * in the order of Marshal declaration (i.e. in the reverse order of inclusion).
+   */
   extenders: RSymbol[];
-  /** true if it is self-referential. */
-  cycle: boolean | undefined;
 };
 export type MarshalHashOptions = {
   defaultValue?: MarshalValue | undefined;
@@ -167,7 +245,6 @@ export type MarshalHashOptions = {
   className?: RSymbol | undefined;
   ivars?: Map<RSymbol, MarshalValue> | undefined;
   extenders?: RSymbol[] | undefined;
-  cycle?: boolean | undefined;
 };
 export function MarshalHash(
   entries: [MarshalValue, MarshalValue][],
@@ -179,7 +256,6 @@ export function MarshalHash(
     className,
     ivars = new Map(),
     extenders = [],
-    cycle,
   } = options;
   return {
     type: "Hash",
@@ -189,40 +265,60 @@ export function MarshalHash(
     className,
     ivars,
     extenders,
-    cycle,
   };
 }
 
 /**
  * Represents an instance of String
  * or its subclass.
+ *
+ * The object may contain cyclic references through its ivars.
  */
 export type MarshalString = {
   type: "String";
+  /**
+   * The bytes of the string.
+   *
+   * It may contain byte sequences that deem invalid
+   * in the encoding.
+   */
   bytes: Uint8Array;
+  /**
+   * The encoding of the string.
+   *
+   * For strings that originate from Ruby <= 1.8,
+   * it is ASCII-8BIT.
+   */
   encoding: REncoding;
   /**
+   * The fully-qualified class name of the object.
+   *
    * Missing className indicates that the object is
    * a direct instance of String.
    */
   className: RSymbol | undefined;
+  /**
+   * Instance variables of the object, in the order of definition.
+   */
   ivars: Map<RSymbol, MarshalValue>;
+  /**
+   * The fully qualified class names of the modules
+   * that the object's singleton class includes,
+   * in the order of Marshal declaration (i.e. in the reverse order of inclusion).
+   */
   extenders: RSymbol[];
-  /** true if it is self-referential. */
-  cycle: boolean | undefined;
 };
 export type MarshalStringOptions = {
   className?: RSymbol | undefined;
   ivars?: Map<RSymbol, MarshalValue> | undefined;
   extenders?: RSymbol[] | undefined;
-  cycle?: boolean | undefined;
 };
 export function MarshalString(
   bytes: Uint8Array,
   encoding: REncoding,
   options: MarshalStringOptions = {},
 ): MarshalString {
-  const { className, ivars = new Map(), extenders = [], cycle } = options;
+  const { className, ivars = new Map(), extenders = [] } = options;
   return {
     type: "String",
     bytes,
@@ -230,16 +326,21 @@ export function MarshalString(
     className,
     ivars,
     extenders,
-    cycle,
   };
 }
 
 /**
  * Represents an instance of Regexp
  * or its subclass.
+ *
+ * The object may contain cyclic references through its ivars,
+ * but for recent versions of Ruby, it may lead to a load error.
  */
 export type MarshalRegexp = {
   type: "Regexp";
+  /**
+   * The bytes of the source string.
+   */
   sourceBytes: Uint8Array;
   /**
    * The encoding of the source string.
@@ -256,8 +357,8 @@ export type MarshalRegexp = {
    * If ruby18Compat is true, the encoding should be
    * one of the following:
    *
-   * - US-ASCII
-   * - ASCII-8BIT
+   * - US-ASCII (only when sourceBytes are all ASCII characters)
+   * - ASCII-8BIT (except when sourceBytes are all ASCII characters)
    * - EUC-JP
    * - Windows-31J
    * - UTF-8
@@ -280,16 +381,33 @@ export type MarshalRegexp = {
    * $KCODE value at runtime is preferred.
    */
   noEncoding: boolean;
+  /**
+   * Indicates that the Regexp object originates from Ruby <= 1.8.
+   * Regexp is the only class that supported multiple Kanji codes
+   * (called encodings nowadays) in Ruby <= 1.8, and ruby18Compat
+   * indicates that the encoding is saved in the old format.
+   *
+   * It also indicates that some of the escapes should be interpreted
+   * differently.
+   */
   ruby18Compat: boolean;
   /**
+   * The fully-qualified class name of the object.
+   *
    * Missing className indicates that the object is
    * a direct instance of Regexp.
    */
   className: RSymbol | undefined;
+  /**
+   * Instance variables of the object, in the order of definition.
+   */
   ivars: Map<RSymbol, MarshalValue>;
+  /**
+   * The fully qualified class names of the modules
+   * that the object's singleton class includes,
+   * in the order of Marshal declaration (i.e. in the reverse order of inclusion).
+   */
   extenders: RSymbol[];
-  /** true if it is self-referential. */
-  cycle: boolean | undefined;
 };
 export type MarshalRegexpOptions = {
   ignoreCase?: boolean | undefined;
@@ -300,7 +418,6 @@ export type MarshalRegexpOptions = {
   className?: RSymbol | undefined;
   ivars?: Map<RSymbol, MarshalValue> | undefined;
   extenders?: RSymbol[] | undefined;
-  cycle?: boolean | undefined;
 };
 export function MarshalRegexp(
   sourceBytes: Uint8Array,
@@ -316,7 +433,6 @@ export function MarshalRegexp(
     className,
     ivars = new Map(),
     extenders = [],
-    cycle,
   } = options;
   return {
     type: "Regexp",
@@ -330,13 +446,15 @@ export function MarshalRegexp(
     className,
     ivars,
     extenders,
-    cycle,
   };
 }
 
 /**
  * Represents an instance of a class
  * that has `marshal_dump` and `marshal_load` methods.
+ *
+ * The object may not contain cyclic references to itself
+ * in its inner dump value.
  */
 export type MarshalDump = {
   type: "#marshal_dump";
@@ -359,6 +477,9 @@ export function MarshalDump(
 /**
  * Represents an instance of a class
  * that has `_dump` and `_load` methods.
+ *
+ * The object may not contain cyclic references to itself
+ * in its inner dump value (which is only possible via its ivars).
  */
 export type MarshalDumpBytes = {
   type: "#_dump";
@@ -373,18 +494,33 @@ export type MarshalDumpBytes = {
    * the String argument of `klass._load`.
    */
   encoding: REncoding;
+  /**
+   * The instance variables of the object, in the order of definition.
+   * The current CRuby implementation has buggy loading behavior,
+   * so you should avoid using this field.
+   */
+  // TODO: implement ivars
+  ivars: Map<RSymbol, MarshalValue>;
+};
+export type MarshalDumpBytesOptions = {
+  ivars?: Map<RSymbol, MarshalValue> | undefined;
 };
 export function MarshalDumpBytes(
   className: RSymbol,
   bytes: Uint8Array,
   encoding: REncoding,
+  options: MarshalDumpBytesOptions = {},
 ): MarshalDumpBytes {
-  return { type: "#_dump", className, bytes, encoding };
+  const { ivars = new Map() } = options;
+  return { type: "#_dump", className, bytes, encoding, ivars };
 }
 
 /**
  * Represents an instance of an extension-defined class
  * that has `_dump_data` and `_load_data` methods.
+ *
+ * The object may not contain cyclic references to itself
+ * in its inner dump value.
  */
 export type MarshalDumpData = {
   type: "#_dump_data";
@@ -407,24 +543,37 @@ export function MarshalDumpData(
 /**
  * Represents an instance of a subclass
  * of Struct or Data (>= Ruby 3.2).
+ *
+ * The object may contain cyclic references through its entries
+ * or ivars.
  */
 export type MarshalStruct = {
   type: "Struct";
   className: RSymbol;
+  /**
+   * The entries of the struct, in the order of definition in
+   * Struct.new or Data.define.
+   *
+   * When loading, the order of field names must match between
+   * the definition and the data to be loaded.
+   */
   entries: [RSymbol, MarshalValue][];
-  /** true if it is self-referential. */
-  cycle: boolean | undefined;
+  /**
+   * Instance variables of the object, in the order of definition.
+   */
+  // TODO: implement ivars
+  ivars: Map<RSymbol, MarshalValue>;
 };
 export type MarshalStructOptions = {
-  cycle?: boolean | undefined;
+  ivars?: Map<RSymbol, MarshalValue> | undefined;
 };
 export function MarshalStruct(
   className: RSymbol,
   entries: [RSymbol, MarshalValue][],
   options: MarshalStructOptions = {},
 ): MarshalStruct {
-  const { cycle } = options;
-  return { type: "Struct", className, entries, cycle };
+  const { ivars = new Map() } = options;
+  return { type: "Struct", className, entries, ivars };
 }
 
 /**
@@ -441,6 +590,8 @@ export type MarshalModule = {
    */
   kind: "class" | "module" | "legacy";
   /**
+   * The fully-qualified name of the class or module itself.
+   *
    * Due to format limitations, the module name can only be in
    * US-ASCII or ASCII-8BIT encoding.
    */
